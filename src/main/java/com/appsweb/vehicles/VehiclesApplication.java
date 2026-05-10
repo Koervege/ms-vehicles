ae2859254f170e6287177e471ccaa3f571f6cbda
Carlos Arce <carlos_arce93@hotmail.com>
2026-05-10 14:58:41 -0500

    add -parameters compile flag. Also moves form groovy to java

--- src/main/java/com/appsweb/vehicles/VehiclesApplication.java
+++ src/main/java/com/appsweb/vehicles/VehiclesApplication.java
@@ -1,21 +1,19 @@
-package com.appsweb.vehicles
+package com.appsweb.vehicles;
 
-import org.springframework.boot.SpringApplication
-import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.cloud.client.loadbalancer.LoadBalanced
-import org.springframework.context.annotation.Bean
-import org.springframework.web.client.RestTemplate
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-class VehiclesApplication {
-
-    static void main(String[] args) {
-        SpringApplication.run(VehiclesApplication, args)
+public class VehiclesApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(VehiclesApplication.class, args);
     }
 
     @Bean
-    //@LoadBalanced
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
+
 }
